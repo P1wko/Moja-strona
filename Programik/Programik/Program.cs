efe88849f2dbf1a6e846611478be29b28b2f10db
P1wko <miminios20611@gmail.com>
2022-05-31 22:36:13 +0200

    cos

--- Programik/Programik/Program.cs
+++ Programik/Programik/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Witam!");
+﻿using System;
+
+namespace Programik
+{
+    class Program
+    {
+        static void Main
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
